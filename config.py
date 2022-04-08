2a4b54f6f8d209f85a58d63cac6d529561d96ba2
kotovtimur1990 <kotov.timur.1990@mail.ru>
2022-04-08 11:35:06 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 BOT_TOKEN = "5267048474:AAGWGYRMKk9Ab4pAs9TGrTa2QjZjCsAx9QM"
 APP_URL = "https://bearandbuls.herokuapp.com/" + BOT_TOKEN
-DB_URI = "postgres://evzafimcndjodp:ccf25baab38dc8b65292ecc7a389ca8584f466198a48a6d779f9fa0fb6d45e27@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/d9pofjotqjelp3"
+#DB_URI = "postgres://evzafimcndjodp:ccf25baab38dc8b65292ecc7a389ca8584f466198a48a6d779f9fa0fb6d45e27@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/d9pofjotqjelp3"
 apikey = "4dd7cd0a-35ca-45bc-ac71-fb8f2a9903d3"
